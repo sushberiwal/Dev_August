1ceda170c74010c2a9360b47bcc79defec3d1c51
sushberiwal <sushantberiwal@gmail.com>
2020-08-19 18:30:03 +0530

    allMatches Links

--- Lec2_JS_activity/activity/activity.js
+++ Lec2_JS_activity/activity/activity.js
@@ -1,10 +1,14 @@
 let request = require("request");
 let fs = require("fs");
 let cheerio = require("cheerio");
+
+const allMatches = require("./allMatches");
+
+
 request( "https://www.espncricinfo.com/series/_/id/8039/season/2015/icc-cricket-world-cup " , callback );
 
 function callback( error , response , data ){
-   //succesfull data received
+    //succesfull data received
     if(error == null && response.statusCode == 200){
         parseData(data);
     }
@@ -24,6 +28,7 @@ function parseData(data){
     let ch = cheerio.load(data);
     let link = ch(".widget-items.cta-link a").attr("href");
     let completeLink = "https://www.espncricinfo.com"+link;
-    console.log(completeLink);
+    
+    allMatches(completeLink);
 }
 
