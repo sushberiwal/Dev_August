c149be2fc863cfefdedd8414ddac5354c0fadc0d
sushberiwal <sushantberiwal@gmail.com>
2020-09-01 18:49:18 +0530

    on challenge create page

--- Lec5_hkChallenge/activity/login_puppeteer.js
+++ Lec5_hkChallenge/activity/login_puppeteer.js
@@ -3,33 +3,68 @@
 const puppeteer = require("puppeteer");
 // object destructring
 const {id , pw} = require("./credentials");
-
+const challenges = require("./challenges");
 // by default puppeteer runs in headless mode
+let gTab;
 
 (async function(){
-    // buid browser
-    const browser = await puppeteer.launch(
-        {
-            headless:false,
-            defaultViewport:null,
-            slowMo:100,
-            args:["--start-maximized"]
-        }
-    );
 
-    // build tab/page
-    const pages = await browser.pages();
-    const page = pages[0];
+    try{
+        // buid browser
+        const browser = await puppeteer.launch(
+            {
+                headless:false,
+                defaultViewport:null,
+                slowMo:50,
+                args:["--start-maximized"]
+            }
+        );
+    
+        // build tab/page
+        const pages = await browser.pages();
+        const page = pages[0];
+        gTab = page;
+        //go to page
+        await page.goto('https://www.hackerrank.com/auth/login');
+        await page.type("#input-1" , id);
+        await page.type("#input-2" , pw);
+        await clickAndWait(".ui-btn.ui-btn-large.ui-btn-primary.auth-button");
+        console.log("Logged in !!");
 
-    //go to page
-    await page.goto('https://www.hackerrank.com/auth/login');
-    await page.type("#input-1" , id);
-    await page.type("#input-2" , pw);
-    await page.click(".ui-btn.ui-btn-large.ui-btn-primary.auth-button");
-    console.log("Logged in !!");
-    await page.waitForSelector('a[data-analytics="NavBarProfileDropDown"]' , {visible:true});
-    await page.click('a[data-analytics="NavBarProfileDropDown"]');    
-    await page.waitForSelector('a[data-analytics="NavBarProfileDropDownAdministration"]' , {visible:true});
-    await page.click('a[data-analytics="NavBarProfileDropDownAdministration"');
-    console.log("Clicked on administration");
+        await page.click('a[data-analytics="NavBarProfileDropDown"]');
+        await clickAndWait('a[data-analytics="NavBarProfileDropDownAdministration"]');
+        console.log("Clicked on administration");
+
+        let bothItems = await page.$$(".nav-tabs.nav.admin-tabbed-nav li");
+        // Array => 2 elements [ <li> </li> , <li></li> ] ;
+
+        // console.log(bothItems[1]);
+        // console.log(bothItems.length);
+        await bothItems[1].click()
+        let challengeUrl = await page.url();
+        console.log(challengeUrl);
+        await clickAndWait(".btn.btn-green.backbone.pull-right");
+        await createChallenge(challenges[0]);
+    }
+    catch(err){
+        console.log(err);
+    }
 })();
+
+
+async function clickAndWait(selector){
+    try{
+       await Promise.all(  [gTab.click(selector) , gTab.waitForNavigation({waitUntil:"networkidle0"}) ] );
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+async function createChallenge(ch){
+    
+    await gTab.waitForSelector("#name" , {visible:true});
+    await gTab.type("#name" , ch["Challenge Name"]);
+    await gTab.type("#preview" , ch["Description"]);
+    await gTab.type("#problem_statement-container .CodeMirror textarea" , ch["Problem Statement"]);
+}
