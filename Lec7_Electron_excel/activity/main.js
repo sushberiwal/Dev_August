2db050721a44edb5af66690ad35bfff3d683fdcc
sushberiwal <sushantberiwal@gmail.com>
2020-09-07 20:09:11 +0530

    completed layout of cells

--- Lec7_Electron_excel/activity/main.js
+++ Lec7_Electron_excel/activity/main.js
@@ -3,6 +3,8 @@
 // npm init -y
 // npm install electron --save-dev
 // add "start":"electron ." in package.json file
+// sudo npm install ejs-electron
+
 
 // html css javascript/jquery
 
@@ -33,3 +35,22 @@ function createWindow(){
 }
 app.whenReady().then(createWindow);
 
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
+  })
+  
+  app.on('activate', () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+  
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
