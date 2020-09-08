8fc31d091eae205da3628029d5fee5b12a7cabbc
sushberiwal <sushantberiwal@gmail.com>
2020-09-08 17:28:04 +0530

    address shown

---
+++ Lec7_Electron_excel/activity/script.js
@@ -0,0 +1,20 @@
+let $ = require("jquery");
+
+
+$(document).ready(function(){
+    
+    console.log("jquery is loaded");
+
+    // event attach cell => click 
+    $("#cells #cell").on("click" , function(){
+
+        let rowId = Number($(this).attr("r-id"));
+        let colId = Number($(this).attr("c-id"));
+
+        // A1 // B2
+        let address =  String.fromCharCode(colId+65) + (rowId+1);
+        // console.log(address);
+        // console.log(rowId , colId);
+        $("#address").val(address);
+    })
+})
---
+++ test.js
@@ -0,0 +1,8 @@
+let ch = "A1";
+
+let character = ch[0];
+console.log(character);
+
+if(character >= "A" && character <= "Z"){
+    
+}
