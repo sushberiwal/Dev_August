bac5b187cc2ffc459b5912f2b3de6c36341e035f
sushberiwal <sushantberiwal@gmail.com>
2020-10-21 20:04:23 +0530

    task list complete

--- Lec10_Insta_FullStack/FrontEnd/todo/src/App.js
+++ Lec10_Insta_FullStack/FrontEnd/todo/src/App.js
@@ -6,25 +6,46 @@ import TaskList from './components/TaskList';
 
 
 class App extends Component {
-  
+
   state = {
-    task : ""
-    }
-  
-  addTasks = (task)=>{
+    tasks: [
+      { id: 1, task: "Learn ES6" },
+      { id: 2, task: "Learn React" },
+      { id: 3, task: "Learn javascript" },
+      { id: 4, task: "Learn angular" }
+    ],
+  };
+
+  deleteTask = (id) => {
+    console.log("delete handle function called !!");
+    let filteredTasks = this.state.tasks.filter((task) => {
+      return task.id != id;
+    });
     this.setState({
-      task:task
-    })
+      tasks: filteredTasks,
+    });
+  };
+
+  
+  addTask = (task) =>{
+    if(task){
+      let tasks = this.state.tasks; // [  obj , obj , obj , obj    ];
+      let newTask = {id : tasks.length+1 , task:task};
+      let newTasks = [ ...tasks , newTask ];
+      this.setState({
+          tasks : newTasks
+      })
+    }
   }
 
-  
 
   render() { 
-    let newTask = this.state.task;
+    let tasks = this.state.tasks;
+    let deleteTask = this.deleteTask;
     return ( 
       <React.Fragment>
-    <InputBox handleAddTask = { this.addTasks } />
-    <TaskList newTask = {newTask}/>    
+    <InputBox handleAddTask = { this.addTask } />
+    <TaskList tasks = {tasks} handleDeleteTask = {deleteTask} />    
     </React.Fragment>
      );
   }
--- Lec10_Insta_FullStack/FrontEnd/todo/src/components/TaskList.jsx
+++ Lec10_Insta_FullStack/FrontEnd/todo/src/components/TaskList.jsx
@@ -4,46 +4,17 @@ import Task from "./Task";
 // class component => cc
 
 class TaskList extends Component {
-  state = {
-    tasks: [
-      { id: 1, task: "Learn ES6" },
-      { id: 2, task: "Learn React" },
-      { id: 3, task: "Learn javascript" },
-      { id: 4, task: "Learn angular" }
-    ],
-  };
-
-  deleteTask = (id) => {
-    console.log("delete handle function called !!");
-    let filteredTasks = this.state.tasks.filter((task) => {
-      return task.id != id;
-    });
-    this.setState({
-      tasks: filteredTasks,
-    });
-  };
-
-
-  addTask = (task) =>{
-    if(task){
-      let tasks = this.state.tasks; // [  obj , obj , obj , obj    ];
-      let newTask = {id : tasks.length+1 , task:task};
-      let newTasks = [ ...tasks , newTask ];
-      this.setState({
-          tasks : newTasks
-      })
-    }
-  }
-
-  render() {
-    let tasks = this.state.tasks;
   
+  render() {    
+    let tasks = this.props.tasks;
+    let deleteTask = this.props.handleDeleteTask;
+    
     return (
       <React.Fragment>
         {tasks.map((task) => {
           return <Task key={task.id} 
                        task={task} 
-                       handleDeleteTask = {this.deleteTask} />;
+                       handleDeleteTask = {deleteTask} />;
         })}
       </React.Fragment>
     );
