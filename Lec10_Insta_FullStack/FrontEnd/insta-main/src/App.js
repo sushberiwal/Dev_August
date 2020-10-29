c48d5c318e0f82e5f32e6585dac639403f68abd2
sushberiwal <sushantberiwal@gmail.com>
2020-10-29 20:09:46 +0530

    header component

---
+++ Lec10_Insta_FullStack/FrontEnd/insta-main/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+// jsx => js
+
+function App() {
+  return ( <Header /> );
+}
+
+
+
+export default App;
---
+++ Lec10_Insta_FullStack/FrontEnd/insta-main/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return ( 
+        <div className="header">
+            <div className="header-image">
+                <img src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""/>
+            </div>
+        </div>
+      );
+}
+ 
+export default Header;
