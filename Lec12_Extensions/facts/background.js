dcd12d16d7e4002f4562b457cf6e04b1bb0ccf9d
sushberiwal <sushantberiwal@gmail.com>
2020-12-05 17:36:43 +0530

    site closer

--- Lec12_Extensions/facts/background.js
+++ Lec12_Extensions/facts/background.js
@@ -1,1 +1,14 @@
-console.log("hello from background !!!");
+chrome.tabs.onCreated.addListener(function(tab){
+    console.log(tab);
+})
+
+
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    console.log(tab);
+    // console.log(changeInfo);
+    // console.log(tabId);
+    // url = "https://www.youtube.com"
+    if(tab.url.includes("youtube")){
+        chrome.tabs.remove(tabId);
+    }
+})
--- Lec12_Extensions/facts/contentScript.js
+++ Lec12_Extensions/facts/contentScript.js
@@ -2,16 +2,12 @@ let images = ["./images/89125115b0e10b94e3378d484712450727-25-thanos.rsocial.w12
 "./images/97d1d9f934a350cee765c5ac1a466605.jpg" , "./images/Thanos_MCU.jpg"];
 
 function changeImage(){
-    
     let allImages = document.querySelectorAll("img");
-    
     for(let i=0 ; i<allImages.length ; i++){
-    
         let idx = Math.floor(Math.random() * images.length);
         console.log(idx);
         // console.log(chrome.extension.getURL("images/Thanos_MCU.jpg"));
         allImages[i].src = chrome.extension.getURL(images[idx]) ;
-    
     }
 }
 
