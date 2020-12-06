3b243527196a840799369b71918d054e70b8f259
sushberiwal <sushantberiwal@gmail.com>
2020-12-06 16:19:09 +0530

    popup html complete

---
+++ Lec12_Extensions/activity/social_media_blocker/popup.js
@@ -0,0 +1,34 @@
+let blockButton = document.querySelector(".block");
+let site = document.querySelector("#block-site");
+let blockedSiteList = document.querySelector(".list-group");
+
+{
+  /* <li class="list-group-item">Cras justo odio</li> */
+}
+blockButton.addEventListener("click", function () {
+  let value = site.value;
+  if (value) {
+    let li = document.createElement("li");
+    li.classList.add("list-group-item");
+    let i = document.createElement("i");
+    i.classList.add("fas");
+    i.classList.add("fa-trash");
+
+
+    let div = document.createElement("div");
+    div.classList.add("d-flex");
+    div.classList.add("justify-content-between");
+    div.innerHTML += "<p>"+value+"</p>";
+    div.append(i); 
+
+    li.append(div);
+    blockedSiteList.append(li);
+    
+    site.value = "";
+    
+    i.addEventListener("click", function () {
+      i.parentNode.parentNode.remove();
+    });
+  
+}
+});
